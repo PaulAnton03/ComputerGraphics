6f1723ef6ddb976b9da89bfdefdec0fe8fc6bc0e
Thijs Houben <t.h.j.houben@student.tudelft.nl>
2023-11-03 18:37:44 +0100

    feat:give motion debug sphere own method

--- src/draw.h
+++ src/draw.h
@@ -29,5 +29,7 @@ void drawSphere(const Sphere& sphere);
 void drawSphere(const glm::vec3& center, float radius, const glm::vec3& color = glm::vec3(1.0f));
 void drawScene(const Scene& scene);
 void drawTriangleMotion(const Vertex& v0, const Vertex& v1, const Vertex& v2);
+void drawSphereMotion(const Sphere& sphere);
+
 
 
